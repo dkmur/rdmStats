263fd8837cc42a9ecedec7e8a172672824522999
dkmur <dkmur74@gmail.com>
2022-11-07 18:57:36 +0100

    spatial index on temp table + history clearup to 5.sh

---
+++ cron_files/5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+folder="$(cd ../ && pwd)"
+source $folder/config.ini
+
+# Logging
+mkdir -p $folder/logs
+touch $folder/logs/log_$(date '+%Y%m').log
+
+# stderr to logfile
+exec 2>> $folder/logs/log_$(date '+%Y%m').log
+
+# table cleanup pokemon_history
+if [[ ! -z $pokemon_history ]] ;then
+  start=$(date '+%Y%m%d %H:%M:%S')
+  MYSQL_PWD=$sqlpass mysql -h$dbip -P$dbport -u$sqluser $scannerdb -e "SET SESSION tx_isolation = 'READ-UNCOMMITTED'; call historyCleanup();"
+  stop=$(date '+%Y%m%d %H:%M:%S')
+  diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
+  echo "[$start] [$stop] [$diff] cleanup table pokemon_history" >> $folder/logs/log_$(date '+%Y%m').log
+fi
