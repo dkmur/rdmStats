cd058f50e10582278ad09b17c6e7e3cf25af9e21
dkmur <dkmur74@gmail.com>
2022-11-15 11:27:32 +0100

    fix cron and isolation

--- cron_files/5.sh
+++ cron_files/5.sh
@@ -13,7 +13,7 @@ exec 2>> $folder/logs/log_$(date '+%Y%m').log
 # table cleanup pokemon_history
 if [[ ! -z $pokemon_history ]] ;then
   start=$(date '+%Y%m%d %H:%M:%S')
-  MYSQL_PWD=$sqlpass mysql -h$dbip -P$dbport -u$sqluser $scannerdb -e "SET SESSION tx_isolation = 'READ-UNCOMMITTED'; call historyCleanup();"
+  MYSQL_PWD=$sqlpass mysql -h$dbip -P$dbport -u$sqluser $scannerdb -e "SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED; call historyCleanup();"
   stop=$(date '+%Y%m%d %H:%M:%S')
   diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
   echo "[$start] [$stop] [$diff] cleanup table pokemon_history" >> $folder/logs/log_$(date '+%Y%m').log
