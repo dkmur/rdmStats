9079f224538b5386d163d0f1117b1c4b113123f2
dkmur <dkmur74@gmail.com>
2022-12-01 00:00:14 +0100

    initial commit atlas log processing

--- cron_files/1440.sh
+++ cron_files/1440.sh
@@ -30,6 +30,16 @@ then
   echo "[$start] [$stop] [$diff] rpl1440 quest area stats processing" >> $folder/logs/log_$(date '+%Y%m').log
 fi
 
+# rpl 1440 atlas log stats
+if "$atlas_monitor"
+then
+  start=$(date '+%Y%m%d %H:%M:%S')
+  MYSQL_PWD=$sqlpass mysql -u$sqluser -h$dbip -P$dbport $rdmstatsdb < $folder/default_files/1440_atlaslog.sql.default
+  stop=$(date '+%Y%m%d %H:%M:%S')
+  diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
+  echo "[$start] [$stop] [$diff] rpl1440 atlas log processing" >> $folder/logs/log_$(date '+%Y%m').log
+fi
+
 ## Cleanup stats tables
 # cleanup mon_area, spawnpoint and quest
 if [[ ! -z $area_rpl15 ]] ;then
